d87c1bfcfb41b9a04ca17305b7e872d4d62e9425
gcruzgar <g.cruz@leeds.ac.uk>
2019-04-09 14:42:17 +0100

    number of working days taken for translation

--- scripts/WTO_data.py
+++ scripts/WTO_data.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import pandas as pd 
 from datetime import datetime
 
 # Load translation time data
-tr_times = pd.read_excel("data/Tableau_stats.xlsx") 
+orig_df = pd.read_excel("data/Tableau_stats.xlsx") 
 
 # Split columns for french and spanish translations + change column names
-french = tr_times.iloc[2:,3:8].drop("TRANSLATION REVISION", axis=1)
+french = orig_df.iloc[2:,3:8].drop("TRANSLATION REVISION", axis=1)
 french.columns=["TRANSLATION START", "TRANSLATION END", "POOL START", "POOL END"]
-spanish = tr_times.iloc[2:,8:-1].drop("TRANSLATION REVISION.1", axis=1)
+spanish = orig_df.iloc[2:,8:-1].drop("TRANSLATION REVISION.1", axis=1)
 spanish.columns=["TRANSLATION START", "TRANSLATION END", "POOL START", "POOL END"]
 
 # Change string to datetime + correct typos
@@ -26,5 +27,34 @@ spanish["POOL START"].iloc[5] = datetime.strptime(spanish["POOL START"].iloc[5],
 spanish["TRANSLATION START"].iloc[95] = datetime(2018, 5, 23)
 
 # Time taken for translation (excluding weekends)
-french["TIME TAKEN"] = french["TRANSLATION END"] - french["TRANSLATION START"]
-spanish["TIME TAKEN"] = spanish["TRANSLATION END"] - spanish["TRANSLATION START"]
+def working_days(start, end):
+    """Output the number of working days between two given dates.
+    Note: this assumes dates provided work on a Monday to Sunday basis.
+    It does not take into account any holidays. """
+
+    daydiff = end.weekday() - start.weekday() # difference in weekday, eg. Wednesday - Tuesday = -1
+    days = ((end-start).days - daydiff) / 7 * 5 + min(daydiff, 5) - (max(end.weekday() - 4, 0) % 5 ) # convert week to workdays
+
+    return days
+
+day_list = []
+for i in french.index:
+    days = working_days(french["TRANSLATION START"].loc[i], french["TRANSLATION END"].loc[i]) + 1 # +1 so that finishing on the same day it started counts as one day
+    day_list.append(days)
+
+french["TRANSLATION DAYS"] = day_list
+
+day_list = []
+for i in spanish.index:
+    days = working_days(spanish["TRANSLATION START"].loc[i], spanish["TRANSLATION END"].loc[i]) + 1 # +1 so that finishing on the same day it started counts as one day
+    day_list.append(days)
+
+spanish["TRANSLATION DAYS"] = day_list
+
+# join time talen for each language into one df
+time_df = pd.concat([french["TRANSLATION DAYS"], spanish["TRANSLATION DAYS"]], axis=1, sort=False)
+time_df.columns = ["FRENCH DAYS", "SPANISH DAYS"]
+
+# join with number of words in original document
+time_df = pd.concat([orig_df["WORDS"].iloc[2:], time_df], axis=1, sort=False)
+
