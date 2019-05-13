1e24cbfb8a56854e1e7dfab1f92e0a6c15da6e02
gcruzgar <g.cruz@leeds.ac.uk>
2019-05-13 14:34:11 +0100

    combining french and spanish

--- scripts/wto_reg.py
+++ scripts/wto_reg.py
@@ -50,8 +50,83 @@ ols_residuals = y_test - y_pred
 ols.score(X_test_s, y_test)
 
 plt.scatter(y_test, y_pred)
+plt.plot(range(400,2200), range(400,2200), 'k-')
+plt.xlabel('True values')
+plt.ylabel('Predicted values')
+plt.title('Ordinary Least Squares')
+#plt.show()
+
+#Residuals
+plt.figure()
+plt.plot(y_test, ols_residuals, '.')
+plt.xlabel("Real value")
+plt.ylabel("Residual")
+plt.title("OLS Residuals")
+plt.show()
+
+""" 
+Train with French, test with Spanish
+"""
+
+X_train = X
+X_test = X
+y_train = y_fr
+y_test = y_sp
+
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train_s = scaler.transform(X_train)
+
+X_test_s = scaler.transform(X_test)
+
+ols = linear_model.LinearRegression()
+ols.fit(X_train_s, y_train)
+
+y_pred = ols.predict(X_test_s)
+ols_residuals = y_test - y_pred
+
+#print("Coefficients: \n{}".format(ols.coef_))
+ols.score(X_test_s, y_test)
+
+plt.scatter(y_test, y_pred)
 #plt.plot(range(400,2200), range(400,2200), 'k-')
 plt.xlabel('True values')
 plt.ylabel('Predicted values')
 plt.title('Ordinary Least Squares')
 plt.show()
+
+"""
+Combine French and Spanish
+"""
+
+X_combined = pd.concat([X, X], axis=0, ignore_index=True)
+y_combined = pd.concat([y_fr, y_sp], axis=0, ignore_index=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X_combined, y_combined, test_size=0.10, random_state=210) #set random_state for reproducibility
+
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train_s = scaler.transform(X_train)
+
+X_test_s = scaler.transform(X_test)
+
+ols = linear_model.LinearRegression()
+ols.fit(X_train_s, y_train)
+
+y_pred = ols.predict(X_test_s)
+ols_residuals = y_test - y_pred
+
+plt.scatter(y_test, y_pred)
+#plt.plot(range(100,5000), range(100,5000), 'k-')
+plt.xlabel('True values')
+plt.ylabel('Predicted values')
+plt.title('Ordinary Least Squares')
+#plt.show()
+
+#Residuals
+plt.figure()
+plt.plot(y_test, ols_residuals, '.')
+plt.xlabel("Real value")
+plt.ylabel("Residual")
+plt.title("OLS Residuals")
+plt.show()
+
+ols.score(X_test_s, y_test)
