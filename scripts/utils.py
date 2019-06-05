cc11785b4d353d57629a1840be44202d34cbe8fa
gcruzgar <g.cruz@leeds.ac.uk>
2019-06-05 15:09:43 +0100

    making plots optional and returning test sets

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,7 +7,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
-def linear_regression(X, y, test_size=0.2, random_state=123):
+def linear_regression(X, y, test_size=0.2, random_state=123, plots=True):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state) #set random_state for reproducibility
 
@@ -22,32 +22,34 @@ def linear_regression(X, y, test_size=0.2, random_state=123):
     y_pred = ols.predict(X_test_s)
     ols_residuals = y_test - y_pred
 
-    # Real vs Predicted
-    plt.figure()
-    plt.scatter(y_test, y_pred)
-    #plt.plot(range(0, 1), range(0, 1), 'k-')
-    plt.plot(np.unique(y_test), np.poly1d(np.polyfit(y_test, y_pred, 1))(np.unique(y_test)), 'r--')
-    plt.xlabel('True values')
-    plt.ylabel('Predicted values')
-    plt.title('OLS - Real vs Predicted')
+    if plots == True:
 
-    #Residuals
-    plt.figure()
-    plt.plot(y_test, ols_residuals, '.')
-    plt.xlabel("Real value")
-    plt.ylabel("Residual")
-    plt.title("OLS Residuals")
-    plt.show()
+        # Real vs Predicted
+        plt.figure()
+        plt.scatter(y_test, y_pred)
+        #plt.plot(range(0, 1), range(0, 1), 'k-')
+        plt.plot(np.unique(y_test), np.poly1d(np.polyfit(y_test, y_pred, 1))(np.unique(y_test)), 'r--')
+        plt.xlabel('True values')
+        plt.ylabel('Predicted values')
+        plt.title('OLS - Real vs Predicted')
+
+        # Residuals
+        plt.figure()
+        plt.plot(y_test, ols_residuals, '.')
+        plt.xlabel("Real value")
+        plt.ylabel("Residual")
+        plt.title("OLS Residuals")
+        plt.show()
 
     #print("Coefficients: \n{}".format(ols.coef_))
     print("r2-score: %0.4f" % ols.score(X_test_s, y_test))
 
-    return ols, scaler
+    return ols, scaler, X_test, y_test
 
 def remove_outliers(df, filter_var, lq=10, uq=None):
-""" 
-Remove values above upper quantile, uq, and below lower quantile, lq, from dataframe, df, based on column, filter_var.  
-"""
+    """ 
+    Remove values above upper quantile, uq, and below lower quantile, lq, from dataframe, df, based on column, filter_var.  
+    """
     if uq == None:
         uq = 100 - lq
 
