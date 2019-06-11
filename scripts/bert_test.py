50818ad9f64db6400b1b2925d5b8e94f3d015614
gcruzgar <g.cruz@leeds.ac.uk>
2019-06-11 11:31:47 +0100

    merging metadata and text

--- scripts/bert_test.py
+++ scripts/bert_test.py
@@ -18,19 +18,31 @@ from bert import optimization
 from bert import tokenization
 
 def load_directory_data(directory):
-    """Load all files from a directory in a DataFrame."""
-    data = {}
+    """Load all files from a directory into a DataFrame."""
+
+    data = pd.DataFrame()
+    data["filename"] = []
     data["text"] = []
+    i=0
     for file_path in os.listdir(directory):
+
         with tf.gfile.GFile(os.path.join(directory, file_path), "r") as f:
-            data["text"].append(f.read())
-    
-    return pd.DataFrame.from_dict(data)
+            data.loc[i] = [file_path, f.read()]
+
+        i+=1
+
+    return data
 
 # Load data
 metadata = pd.read_csv("data/timed-un/reliable.dat", sep=' ')
-#metadata = remove_outliers(metadata, 'perday', lq=0.05, uq=0.95) # remove outliers
 
 text_path = "data/timed-un/un-readability/"
 text_df = load_directory_data(text_path)
+text_df['filename'] = text_df['filename'].str.lower() # change filenames to lower case to match metadata
 
+# Join all into single dataframe
+df = metadata.merge(text_df, left_on="ID", right_on="filename")
+df.drop(columns=["filename"], inplace=True)
+
+# Remove outliers
+df = remove_outliers(df, 'perday', lq=0.05, uq=0.95) 
