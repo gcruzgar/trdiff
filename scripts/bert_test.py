4af061661266d1927ec853aed137fd15145d99bf
gcruzgar <g.cruz@leeds.ac.uk>
2019-06-11 12:09:14 +0100

    define difficulty class

--- scripts/bert_test.py
+++ scripts/bert_test.py
@@ -15,7 +15,7 @@ from sklearn.model_selection import train_test_split
 import bert
 from bert import tokenization
 from bert import optimization
-from bert import tokenization
+from bert import run_classifier
 
 def load_directory_data(directory):
     """Load all files from a directory into a DataFrame."""
@@ -46,3 +46,28 @@ df.drop(columns=["filename"], inplace=True)
 
 # Remove outliers
 df = remove_outliers(df, 'perday', lq=0.05, uq=0.95) 
+
+# Change regression problem into classification - discuss method and come up with something more rigorous?
+df["class"] = 1 # average 
+df.loc[df["perday"] >= df["perday"].quantile(0.67), "class"] = 0 # easy
+df.loc[df["perday"] <= df["perday"].quantile(0.33), "class"] = 2 # hard
+
+# Split data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(df['text'], df['class'], test_size=0.15, random_state=123) #set random_state for reproducibility
+
+
+"""
+BERT
+"""
+
+# Transform data to match format required by BERT.
+DATA_COLUMN = 'text'
+LABEL_COLUMN = 'class'
+label_list = [0, 1, 2]
+
+# # Create InputExample using BERT constructor
+# train_InputExamples = X_train.apply(lambda x: bert.run_classifier.InputExample(guid=None, text_a = x[DATA_COLUMN], 
+#     text_b = None, label = x[LABEL_COLUMN]))#, axis = 1)
+
+# test_InputExamples = X_test.apply(lambda x: bert.run_classifier.InputExample(guid=None, text_a = x[DATA_COLUMN], 
+#     text_b = None, label = x[LABEL_COLUMN])#, axis = 1)
