703f503b93f52fdd98fb409b3afe6fe5f3776e8d
gcruzgar <g.cruz@leeds.ac.uk>
2019-06-19 10:45:40 +0100

    adding classification function

--- scripts/ter_biber.py
+++ scripts/ter_biber.py
@@ -3,6 +3,9 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
+
+from scripts.utils import remove_outliers
+
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score 
 from sklearn import preprocessing
@@ -10,20 +13,53 @@ from sklearn.model_selection import train_test_split
 
 features = pd.read_csv("data/en-fr-100.dim", sep='\t')
 ter = pd.read_csv("data/en-fr-100-mt_score.txt", sep='\t', header=None)
-ter.columns=['Score']
+ter.columns=['score']
 
 df = pd.concat([features, ter], axis=1) 
 
-drop_outliers = True
-if drop_outliers == True:
-    r_95p = df['Score'].quantile(q=0.95)
-    #r_5p = df['Score'].quantile(q=0.05)
-    df = df.loc[df['Score'] < r_95p]
-    #df = df.loc[df['Score'] > r_5p]
+# Remove outliers
+df = remove_outliers(df, 'score', lq=0.05, uq=0.95) 
 
-X = df.drop(columns=['Score'])
-y = df['Score']
+""" Regression """
+from scripts.utils import linear_regression
 
-from utils import linear_regression
+X = df.drop(columns=['score'])
+y = df['score']
 
 linear_regression(X, y)
+
+""" Classification """
+from sklearn.metrics import accuracy_score 
+from sklearn.neighbors import KNeighborsClassifier
+
+# Classify scores depedning on percentile
+df["class"] = 1 # average 
+df.loc[df["score"] >= df["score"].quantile(0.67), "class"] = 0 # good
+df.loc[df["score"] <= df["score"].quantile(0.33), "class"] = 2 # bad
+
+#print("Number of documents per class: \n{}".format(df["class"].value_counts(sort=False).to_string()))
+
+# Split data into training and tests sets, set random_state for reproducibility
+X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=["score", "class"]), df["class"], test_size=0.2, random_state=42)
+
+# Create classifier
+neigh = KNeighborsClassifier(n_neighbors=5, algorithm='auto')
+
+# Fit classifier to train data
+neigh.fit(X_test, y_test)
+
+# Predict using test data
+y_pred = neigh.predict(X_test)
+y_pred_prob = pd.DataFrame(neigh.predict_proba(X_test)).round(2)
+y_pred_prob.columns = ["prob 0", "prob 1", "prob 2"]
+
+# Evaluate results
+diff = {"good translation": 0, "average translation": 1, "bad translation": 2}
+
+y_res = pd.DataFrame(y_pred, columns=['y_pred'])
+y_res['y_test'] = y_test.values
+
+for key in diff.keys():
+        
+    key_val = y_res.loc[y_res["y_pred"] == diff[key]]
+    print( "Accuracy for %s: %0.2f%%" % ( key, accuracy_score( key_val["y_test"], key_val["y_pred"] ) * 100 ) )
