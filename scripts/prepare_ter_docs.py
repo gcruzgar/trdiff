a2b061c2cd50ae93cd8e784900bdc2744c54275c
gcruzgar <g.cruz@leeds.ac.uk>
2019-06-21 10:00:49 +0100

    reformatting as function with arguments

--- scripts/prepare_ter_docs.py
+++ scripts/prepare_ter_docs.py
@@ -1,33 +1,49 @@
-#!/bin/env python3
+#!/usr/bin/env python3
+
 import re
+import argparse
 
-# Open human translation
-filename1 = "UNv1.0.6way.es"
-with open(filename1, "r") as f:
-	es=f.readlines() 
+def main():
 
-# Open machine translation
-filename2 = "UNv1.0.6way.en-es.mt"
-with open(filename2, "r") as f:
-	mt=f.readlines() 
+	# Open human translation
+	filename1 = args.ht
+	with open(filename1, "r") as f:
+		ht=f.readlines() 
 
-for i in range(0, len(es)):
-	# remove digits
-	es[i] = re.sub("\S*[0-9]\S*", "", es[i])
-	mt[i] = re.sub("\S*[0-9]\S*", "", mt[i])
-	
-	# remove unknowns    
-	mt[i] = re.sub("(\(\s)?\<unk\>(\s\))?", "", mt[i])
-	
-	# Add sentence number for TER score (not on original doc)
-	es[i] = es[i].replace("\n", " (A-"+str(i)+")")
-	mt[i] = mt[i].replace("\n", " (A-"+str(i)+")")
+	# Open machine translation
+	filename2 = args.mt
+	with open(filename2, "r") as f:
+		mt=f.readlines() 
 
-# Save outputs to new file
-with open("es.txt", 'w') as f:
-	for item in es:
-		f.write("%s\n" % item)
+	for i in range(0, len(ht)):
+		# remove digits
+		ht[i] = re.sub("\S*[0-9]\S*", "", ht[i])
+		mt[i] = re.sub("\S*[0-9]\S*", "", mt[i])
+		
+		# remove unknowns    
+		mt[i] = re.sub("(\(\s)?\<unk\>(\s\))?", "", mt[i])
+		
+		# Add sentence number for TER score (not on original doc)
+		ht[i] = ht[i].replace("\n", " (A-"+str(i)+")")
+		mt[i] = mt[i].replace("\n", " (A-"+str(i)+")")
 
-with open("mt.txt", 'w') as f:
-	for item in mt:
-		f.write("%s\n" % item)
+	# Save outputs to new file
+	with open("ht.txt", 'w') as f:
+		for item in ht:
+			f.write("%s\n" % item)
+	print("Human translation saved to ht.txt")
+
+	with open("mt.txt", 'w') as f:
+		for item in mt:
+			f.write("%s\n" % item)
+	print("Human translation saved to mt.txt")
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-ht', type=str, nargs='?',
+		help="human translation file to process (including path from current directory)")
+	parser.add_argument('-mt', type=str, nargs='?',
+		help="machine translation file to process (including path from current directory)")
+	args = parser.parse_args()
+
+	main()
