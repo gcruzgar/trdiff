e8b23d744f60df412413665564f051f419196dd1
gcruzgar <g.cruz@leeds.ac.uk>
2019-06-24 13:47:51 +0100

    combining histogram and kde plot

--- scripts/histograms.py
+++ scripts/histograms.py
@@ -17,34 +17,50 @@ def histogram():
 
     fig, axs = plt.subplots(1, 2, sharey=True)
 
-    axs[0].hist(es_df.iloc[:,0], bins=30, edgecolor='black')
+    axs[0].hist(es_df.iloc[:,0], bins=25, edgecolor='black')
     axs[0].set_xlabel("TER")
     axs[0].set_ylabel("Frequency (sentences)")
     axs[0].set_title("UN Corpus - Spanish Translations")
 
-    axs[1].hist(fr_df.iloc[:,0], bins=30, edgecolor='black')
+    axs[1].hist(fr_df.iloc[:,0], bins=25, edgecolor='black')
     axs[1].set_xlabel("TER")
     #axs[1].ylabel("Frequency (sentences)")
     axs[1].set_title("UN Corpus - French Translations")
 
     plt.show()
-
-histogram()
 
 def KernelDensity():
 
     fig, axs = plt.subplots(1, 2, sharey=True)
 
-    sns.kdeplot(es_df.iloc[:,0], bw=0.1, ax=axs[0], legend=False)
+    sns.kdeplot(es_df.iloc[:,0], bw=0.05, ax=axs[0], legend=False)
     axs[0].set_xlabel("TER")
     axs[0].set_ylabel("Density")
     axs[0].set_title("UN Corpus - Spanish Translations")
 
-    sns.kdeplot(fr_df.iloc[:,0], bw=0.1, ax=axs[1], legend=False)
+    sns.kdeplot(fr_df.iloc[:,0], bw=0.05, ax=axs[1], legend=False)
     axs[1].set_xlabel("TER")
     #axs[1].ylabel("Frequency (sentences)")
     axs[1].set_title("UN Corpus - French Translations")
 
     plt.show()
 
-KernelDensity()
+def combined_plot():
+
+    fig, axs = plt.subplots(1, 2, sharey=True)
+
+    sns.distplot(es_df.iloc[:,0], hist=True, kde=True, bins=25, hist_kws={'edgecolor':'black'}, kde_kws={'bw':0.05}, ax=axs[0])#, label=False)
+    axs[0].set_xlabel("TER")
+    axs[0].set_ylabel("Density")
+    axs[0].set_title("UN Corpus - Spanish Translations")
+
+    sns.distplot(fr_df.iloc[:,0], hist=True, kde=True, bins=25, hist_kws={'edgecolor':'black'}, kde_kws={'bw':0.05}, ax=axs[1])#, label=False)
+    axs[1].set_xlabel("TER")
+    #axs[1].ylabel("Frequency (sentences)")
+    axs[1].set_title("UN Corpus - French Translations")
+
+    plt.show() 
+
+#histogram()
+#KernelDensity()
+combined_plot()
