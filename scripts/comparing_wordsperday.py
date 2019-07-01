1120903ed336c14305603966308cc68e3b789f09
gcruzgar <g.cruz@leeds.ac.uk>
2019-07-01 15:15:07 +0100

    comapring cross-language averages

--- scripts/comparing_wordsperday.py
+++ scripts/comparing_wordsperday.py
@@ -9,26 +9,7 @@ wto_french = remove_outliers(wto, 'PERDAY FRENCH', lq=0.05, uq=0.94).drop(column
 wto_spanish = remove_outliers(wto, 'PERDAY SPANISH', lq=0.05, uq=0.94).drop(columns=['DAYS FRENCH', 'PERDAY FRENCH'])
 
 un = pd.read_csv("data/timed-un/reliable.dat", sep=' ')
-un = remove_outliers(un, 'perday', lq=0.05, uq=0.94)
-
-def histogram():
-
-    fig, axs = plt.subplots(1, 3, sharey=True)
-
-    axs[0].hist(un['perday'], bins=20, edgecolor='black')
-    axs[0].set_xlabel("Translation rate (words per day)")
-    axs[0].set_ylabel("Frequency (documents)")
-    axs[0].set_title("UN timed")
-
-    axs[1].hist(wto_french['PERDAY FRENCH'], bins=25, edgecolor='black')
-    axs[1].set_xlabel("Translation rate (words per day)")
-    axs[1].set_title("WTO timed - French Translations")
-
-    axs[2].hist(wto_spanish['PERDAY SPANISH'], bins=25, edgecolor='black')
-    axs[2].set_xlabel("Translation rate (words per day)")
-    axs[2].set_title("WTO timed - Spanish Translations")
-
-    plt.show()
+#un = remove_outliers(un, 'perday', lq=0.05, uq=0.94)
 
 def combined_plot():
 
@@ -37,7 +18,7 @@ def combined_plot():
     sns.distplot(un['perday'], hist=True, kde=True, bins=20, hist_kws={'edgecolor':'black'}, kde_kws={'bw':200}, ax=axs[0])
     axs[0].set_xlabel("Translation rate (words per day)")
     axs[0].set_ylabel("Density")
-    axs[0].set_title("UN Corpus - Spanish Translations")
+    axs[0].set_title("UN Corpus")
 
     sns.distplot(wto_french['PERDAY FRENCH'], hist=True, kde=True, bins=25, hist_kws={'edgecolor':'black'}, kde_kws={'bw':200}, ax=axs[1])
     axs[1].set_xlabel("Translation rate (words per day)")
@@ -49,8 +30,31 @@ def combined_plot():
 
     plt.show()
 
+def compare_un_wto_averages():
+    # Average average translation rate
+    french = wto['PERDAY FRENCH']
+    spanish = wto['PERDAY SPANISH']
+    wto_ave = (french+spanish)/2
+
+    # Remove outliers
+    iqr = wto_ave.quantile(q=0.75) - wto_ave.quantile(q=0.25)
+    wto_ave = wto_ave[wto_ave > wto_ave.quantile(q=0.5) - 1.5*iqr]
+    wto_ave = wto_ave[wto_ave < wto_ave.quantile(q=0.5) + 1.5*iqr]
+
+    fig, axs = plt.subplots(1, 2, sharey=True)
+
+    sns.distplot(un['perday'], hist=True, kde=True, bins=20, hist_kws={'edgecolor':'black'}, kde_kws={'bw':200}, ax=axs[0])
+    axs[0].set_xlabel("Translation rate (words per day)")
+    axs[0].set_ylabel("Density")
+    axs[0].set_title("UN Corpus - Spanish Translations")
+
+    sns.distplot(wto_ave, hist=True, kde=True, bins=25, hist_kws={'edgecolor':'black'}, kde_kws={'bw':100}, ax=axs[1])
+    axs[1].set_xlabel("Translation rate (words per day)")
+    axs[1].set_title("WTO - Average")
+
+    plt.show()
+
 wto.describe()
 un.describe() 
-
-#histogram()
 combined_plot()
+compare_un_wto_averages()
