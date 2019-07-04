c67ab322c71e8e9810b761b40ffbd402105e86fb
gcruzgar <g.cruz@leeds.ac.uk>
2019-07-04 11:41:01 +0100

    updating comments

--- scripts/download_un_data.sh
+++ scripts/download_un_data.sh
@@ -3,11 +3,12 @@
 # wget -O $2 'https://daccess-ods.un.org/access.nsf/GetFile?Open&DS='$1'&Lang=E&Type=DOC'
 
 # $1 is the document code
-# $2 is the desired output name (dowloads a zip file)
+# $2 is the desired output name (downloads a compressed xml file unless .docx is specified)
 
 # The codes are the same as the file names, except replacing / with _).  
 # Replace Lang=E with Lang=F or Lang=S to download translations. 
 
+# download documents as docx
 for filename in ./data/timed-un/un-readability/*.txt; do 
     code=$(echo "$(basename $filename .txt)" | tr '_' '/')
     wget -O ./data/un_texts/en-es/$(basename $filename .txt).docx 'https://daccess-ods.un.org/access.nsf/GetFile?Open&DS='$code'&Lang=S&Type=DOC'
