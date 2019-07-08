7ade545fc12f9d9af5b9b29290c297dcfa6d92f8
gcruzgar <g.cruz@leeds.ac.uk>
2019-07-08 11:04:52 +0100

    updating to read file containing list of codes

--- scripts/download_un_data.sh
+++ scripts/download_un_data.sh
@@ -8,11 +8,10 @@
 # The codes are the same as the file names, except replacing / with _).  
 # Replace Lang=E with Lang=F or Lang=S to download translations. 
 
-# download documents as docx
-for filename in ./data/timed-un/un-readability/*.txt; do 
-    code=$(echo "$(basename $filename .txt)" | tr '_' '/')
-    wget -O ./data/un_texts/en-es/$(basename $filename .txt).docx 'https://daccess-ods.un.org/access.nsf/GetFile?Open&DS='$code'&Lang=S&Type=DOC'
-done
+# download documents as docx (from a list of codes)
+while IFS= read -r line; do
+    wget -O ./data/un_texts/en-es/$line.docx 'https://daccess-ods.un.org/access.nsf/GetFile?Open&DS='$line'&Lang=S&Type=DOC'
+done < data/un_texts/un-reliability.ids
 
 # Convert to txt:
 # libreoffice --headless --convert-to "txt:Text (encoded):UTF8" $1
@@ -20,3 +19,9 @@ done
 for filename in ./data/un_texts/en-es/*.docx; do
     libreoffice --headless --convert-to "txt:Text (encoded):UTF8" $filename
 done
+
+# # obtaining texts (codes from filenames in directory)
+# for filename in ./data/timed-un/un-readability/*.txt; do 
+#     code=$(echo "$(basename $filename .txt)" | tr '_' '/')
+#     wget -O ./data/un_texts/en-es/$(basename $filename .txt).docx 'https://daccess-ods.un.org/access.nsf/GetFile?Open&DS='$code'&Lang=S&Type=DOC'
+# done
