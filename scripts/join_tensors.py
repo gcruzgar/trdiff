864cc68e64d59ab9468392b74eef1900fda9b1f3
gcruzgar <g.cruz@leeds.ac.uk>
2019-07-10 11:41:28 +0100

    script to concattenate tensors from directory

---
+++ scripts/join_tensors.py
@@ -0,0 +1,31 @@
+import os
+import re
+import torch
+import argparse
+
+def main():
+
+    xlm_path=args.p
+    if xlm_path.endswith("/") == False:
+            xlm_path = xlm_path+"/"
+    print("Loading tensors from %s..." % xlm_path)
+
+    file_list = os.listdir(xlm_path)
+
+    ids=[]  
+    t_all=torch.Tensor()
+    for f in file_list:
+        ids.extend(re.findall("\d+", f))
+        t = torch.load(xlm_path+f)
+        t_all=torch.cat((t_all, t), dim=0)
+
+        torch.save(t_all, "xlm-embeddings-"+min(ids)+"-"+max(ids)+".pt")
+
+if __name__ == "__main__":
+
+    parser= argparse.ArgumentParser()
+    parser.add_argument("-p", type=str, nargs='?', default="xlm-embeddings/",
+            help="path to xlm embeddings")
+    args = parser.parse_args()
+        
+    main()
