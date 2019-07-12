e729736731fe8661fcf7a3c38d11f447f0c8761f
gcruzgar <g.cruz@leeds.ac.uk>
2019-07-12 15:03:39 +0100

    reformatting as function

--- scripts/pad_tensor.py
+++ scripts/pad_tensor.py
@@ -1,15 +1,31 @@
 import torch
+import argparse
 
-t_path="xlm-embeddings-810000-820000"
+def main():
 
-ind = 4500
+    #t_path="xlm-embeddings-810000-820000"
+    t1 = args.t
+    t2 = t1+10000
+    t_path="xlm-embeddings-"+str(t1)+str(t2)
+    
+    #ind = 4500
+    ind = args.i
 
-t = torch.load(t_path+".pt")
+    t = torch.load(t_path+".pt")
 
-first_half = t[0:ind]
-second_half = t[ind:]
-pad = torch.zeros(500, 1024)
+    first_half = t[0:ind]
+    second_half = t[ind:]
+    pad = torch.zeros(500, 1024)
 
-t_new = torch.cat((first_half, pad, second_half))
+    t_new = torch.cat((first_half, pad, second_half))
 
-torch.save(t_new, t_path+"-padded.pt")
+    torch.save(t_new, t_path+"-padded.pt")
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", type=int, nargs='?', help='start index of tensor to read')
+    parser.add_argument("-i", type=int, nargs='?', help='start index of missing values')
+    args = parser.parse_args()
+
+    main()    
