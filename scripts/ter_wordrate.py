23b6c4b117da7e52eaf3e40b06458471f3a675ee
gcruzgar <g.cruz@leeds.ac.uk>
2019-07-22 13:50:39 +0100

    working on correlation with xlm

--- scripts/ter_wordrate.py
+++ scripts/ter_wordrate.py
@@ -1,9 +1,7 @@
+import torch
+import numpy as np
 import pandas as pd 
-from scripts.utils import remove_outliers
-
-# Load TER scores
-ter = pd.read_csv("data/en-es-gs.score", header=None, sep='\t')
-ter.columns = ["score"]
+from scripts.utils import remove_outliers, linear_regression
 
 # Load time taken to translate and calculate sentence length
 wpd = pd.read_csv("data/en-es.pe", sep='\t').drop_duplicates()
@@ -14,6 +12,11 @@ for i in wpd.index:
 # Filter empty sentences (with only serial number)
 time = wpd.loc[~wpd['Segment'].str.contains("^\s*\S*[0-9]\S*\s*$"), ['Time-to-edit', 'words']].reset_index(drop=True)
 time.columns= ["time (ms)", "words"]
+
+""" TER - words per day"""
+# Load TER scores
+ter = pd.read_csv("data/en-es-gs.score", header=None, sep='\t')
+ter.columns = ["score"]
 
 # Join important columns to single dataframe
 df = pd.concat([ter, time], axis=1)
@@ -27,3 +30,10 @@ df = remove_outliers(df, 'rate', lq=0.05, uq=0.95)
 
 # Correlation
 print(df.corr().round(3)['score'])
+
+""" XLM - words per day """
+# Load sentece embeddings
+features = pd.DataFrame(torch.load("data/gs-xlm-embeddings.pt").data.numpy())
+reg_df = df.merge(features, left_index=True, right_index=True)
+
+#ols, scaler, X_test, y_test = linear_regression(reg_df[], reg_df['perms'])
