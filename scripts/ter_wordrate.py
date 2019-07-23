8bba7295cfd0553ae3986fcef65acc7709b7fb85
gcruzgar <g.cruz@leeds.ac.uk>
2019-07-23 16:13:42 +0100

    adding option to remove perfect translations

--- scripts/ter_wordrate.py
+++ scripts/ter_wordrate.py
@@ -15,7 +15,7 @@ time.columns= ["time (ms)", "words"]
 
 """ TER - words per day"""
 # Load TER scores
-ter = pd.read_csv("data/golden-standard/en-es-gs.score", header=None, sep='\t')
+ter = pd.read_csv("data/golden-standard/en-fr-gs.score", header=None, sep='\t')
 ter.columns = ["score"]
 
 # Join important columns to single dataframe
@@ -25,11 +25,14 @@ df = pd.concat([ter, time], axis=1)
 df['perms'] = df['words'] / df['time (ms)']
 df['rate'] = (df['perms'] - df['perms'].min()) / (df['perms'].max() - df['perms'].min())
 
+# Remove perfect translations
+dft = df.loc[df['score'] != 0]
+
 # Remove outliers
 dfr = remove_outliers(df, 'rate', lq=0.05, uq=0.95)
 
 # Correlation
-print(dfr.corr().round(3)['score'])
+print(dft.corr().round(3)['score'])
 
 # Quantiles
 q1 = df.loc[df['perms'] <= df['perms'].quantile(0.25)]
@@ -45,7 +48,7 @@ for q in range(1,5):
 qcor_df = pd.DataFrame.from_dict(q_corr)
 qcor_df.columns=['q1', 'q2', 'q3', 'q4']
 
-print(qcor_df)
+print(qcor_df.round(3))
 
 #plots
 import matplotlib.pyplot as plt
